2b18c68674080896e05c74330a5a68c8116de710
Sagar Kumar Choudhary <sagarchoudharyaot@gmail.com>
2025-04-09 16:30:36 +0530

    included speed Insights from vercel

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './index.css';
 import { AuthProvider } from './context/AuthContext';
-import { BrowserRouter } from 'react-router-dom'; // ✅ Moved here
+import { BrowserRouter } from 'react-router-dom';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       {' '}
-      {/* ✅ Wrap here */}
       <AuthProvider>
+        <SpeedInsights />
         <App />
       </AuthProvider>
     </BrowserRouter>
