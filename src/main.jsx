018ac3d8086368619b31fe94f395bbc6bff17e7e
Sagar Kumar Choudhary <sagarchoudharyaot@gmail.com>
2025-04-09 16:46:59 +0530

    fixing speed Insights

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       {' '}
       <AuthProvider>
-        <SpeedInsights />
         <App />
       </AuthProvider>
     </BrowserRouter>
